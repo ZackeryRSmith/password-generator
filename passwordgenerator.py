afd0dc775c7f20d1d5249d6f49bd161fc3ea4dfd
Ori <45724082+therealOri@users.noreply.github.com>
2022-05-20 12:58:52 -0600

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -20,7 +20,7 @@ def start():
           / _ \/ _ `(_-<(_-< |/|/ / _ \/ __/ _  / / _ `/ -_) _ \/ -_) __/ _ `/ __/ _ \/ __/
          / .__/\_,_/___/___/__,__/\___/_/  \_,_/  \_, /\__/_//_/\__/_/  \_,_/\__/\___/_/   
         /_/                                      /___/                                        
-                              {Fore.CYAN + Style.BRIGHT} Made by https://github.com/lucadado """)
+                                                      {Fore.CYAN + Style.BRIGHT} Made by https://github.com/lucadado """)
 
     
     
