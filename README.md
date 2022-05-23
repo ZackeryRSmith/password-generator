09e709bf2b2c4e0f87b461bc3d03b429493b918f
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-05-22 23:17:52 -0400

    :recycle: Code refactor
    Code refactor, more info on changes detailed in the pull request.

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
-# password-generator
-Password Generator in python
+# Minimal password generator
+a small password generator written in python :)
 
-![photo_2022-04-23_11-33-34](https://user-images.githubusercontent.com/43071315/164888982-6624f7bb-8165-4f2b-852d-c01cd7ef57c8.jpg) 
-![photo_2022-04-23_11-33-41](https://user-images.githubusercontent.com/43071315/164889142-8ba1636e-37f7-4604-9f4e-d0786a87b79c.jpg)
-
-# Setup
+### Setup
 ```
 git clone https://github.com/lucadado/password-generator.git
 cd password-generator
@@ -12,3 +9,11 @@ pip install -r requirements.txt
 py passwordgenerator.py
 ```
 
+### Screenshots
+![photo_2022-04-23_11-33-34](https://user-images.githubusercontent.com/43071315/164888982-6624f7bb-8165-4f2b-852d-c01cd7ef57c8.jpg) 
+![photo_2022-04-23_11-33-41](https://user-images.githubusercontent.com/43071315/164889142-8ba1636e-37f7-4604-9f4e-d0786a87b79c.jpg)
+
+### Credits
+* [**lucadado** Luca](https://github.com/lucadado) | Original author
+* [**therealOri** Ori](https://github.com/therealOri) | Formatting and Compatibility
+* [**ZackeryRSmith**](https://github.com/ZackeryRSmith) | Optimizations and Simplifications
--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,77 +1,69 @@
-import secrets
 import os
-import platform
+import re
+import secrets
+import readline  # Required to stop escape sequences from displaying in input
 from colorama import *
+init(autoreset=True)
 
-def clear():
-  os.system('cls||clear')  
-  
-def back():
-    input("\n\nPress enter to continue...")
-    clear()
-    start()
-
-
+def clear(): os.system('cls||clear')  
 def start():
-    init(autoreset=True)
+    clear()
     print(f"""{Fore.LIGHTBLUE_EX}
                                                 __                             __          
            ___  ___ ____ ____    _____  _______/ / ___ ____ ___  ___ _______ _/ /____  ____
           / _ \/ _ `(_-<(_-< |/|/ / _ \/ __/ _  / / _ `/ -_) _ \/ -_) __/ _ `/ __/ _ \/ __/
          / .__/\_,_/___/___/__,__/\___/_/  \_,_/  \_, /\__/_//_/\__/_/  \_,_/\__/\___/_/   
         /_/                                      /___/                                        
-                                                      {Fore.CYAN + Style.BRIGHT} Made by https://github.com/lucadado """)
+        \t\t\t\t\t\t{Fore.CYAN + Style.BRIGHT} Made by https://github.com/lucadado\n\n\n""")
 
+    # Obtain a string of all printable characters
+    printable = "".join(map(chr, range(33, 127)))
     
     
-    letters = "abcdefghijklmnopqrstuvwxyz"
-    numbers = "1234567890"
-    symbols = ":;-_?!.,£$%&/()^*ç°§€<>+#@{[}]"
-    upper = letters.upper()
-    print("\n\n\n")
+    ###################
+    # User input      #
+    ###################
+
+    while True:
+        try:
+            length = int(input(f"{Fore.LIGHTBLUE_EX}Password Length: {Fore.RESET}"))
+        except ValueError:
+            print(f"{Fore.LIGHTRED_EX}Please provide a valid number! E.g. 1, 2, 3, 4..."); continue
+        except KeyboardInterrupt: 
+            print("")  # Fixes partial line issue with ZSH 
+            exit(0)
+        break
+
+    while True:
+        symbask = str(input(f"{Fore.LIGHTBLUE_EX}Do you want symbols? [Y, n]{Fore.RESET} ")).lower()
     
-    
-    try:
-        length = int(input(f"{Fore.LIGHTBLUE_EX}Password Length: {Fore.RESET}"))
-    except ValueError:
-        print(f"\n{Fore.LIGHTRED_EX}Please provide a valid number!")
-        back()
-    symbask = str(input(f"\n{Fore.LIGHTBLUE_EX}Do you want symbols? (yes/no): {Fore.RESET}"))
-    
-    
-    if symbask.lower() == "yes":        
-        total = letters + numbers + upper + symbols
-        password = "".join(secrets.choice(total) for _ in range(length))
-        print(f"\n{Fore.LIGHTBLUE_EX}Password: {Fore.LIGHTYELLOW_EX}{password}")
-        
-    elif symbask.lower() == "no":
-        total = letters + numbers + upper
-        password = "".join(secrets.choice(total) for _ in range(length))
-        print(f"\n{Fore.LIGHTBLUE_EX}Password: {Fore.LIGHTYELLOW_EX}{password}")
-    else:
-        print(f"\n{Fore.LIGHTRED_EX}Please provide a valid answer! (Yes/No)")
-        back() 
-        
-        
-    # RATING
-    if len(password) >= 32:
-        print(f"\n{Fore.LIGHTBLUE_EX}Password Strength: {Fore.MAGENTA}Area51")   
-    elif len(password) >= 26:
-        print(f"\n{Fore.LIGHTBLUE_EX}Password Strength: {Fore.LIGHTGREEN_EX}Very Strong")
-    elif len(password) >= 20:
-        print(f"\n{Fore.LIGHTBLUE_EX}Password Strength: {Fore.LIGHTGREEN_EX}Strong")
-    elif len(password) >= 16:
-        print(f"\n{Fore.LIGHTBLUE_EX}Password Strength: {Fore.GREEN}Good")
-    elif len(password) >= 10:
-        print(f"\n{Fore.LIGHTBLUE_EX}Password Strength: {Fore.LIGHTGREEN_EX}Great")
-    elif len(password) >= 6:
-        print(f"\n{Fore.LIGHTBLUE_EX}Password Strength: {Fore.LIGHTRED_EX}Weak")
-    else:
-        print(f"\n{Fore.LIGHTBLUE_EX}Password Strength: {Fore.RED}Very Weak")
-    back()
+        if symbask in ("yes", "y", ""):        
+            password = "".join(secrets.choice(printable) for _ in range(length))
+            print(f"{Fore.LIGHTBLUE_EX}Password: {Fore.LIGHTYELLOW_EX}{password}")
+            break
+        elif symbask in ("no", "n"):
+            password = "".join(secrets.choice(re.sub(r"[^\w]|_", "", printable)) for _ in range(length))
+            print(f"{Fore.LIGHTBLUE_EX}Password: {Fore.LIGHTYELLOW_EX}{password}")
+            break
+        else:
+            print(f"{Fore.LIGHTRED_EX}Please provide a valid answer! y(es) or n(o)")
+            continue
 
 
+    ###################
+    # Rating          #
+    ###################
 
-if __name__ == '__main__':
-    start()
- 
+    # RATING  -- Could be improved as grading a password souly on length is incomplete.
+    tmp = {
+        32:f"{Fore.LIGHTBLUE_EX}Password Strength: {Fore.MAGENTA}Area51",
+        26:f"{Fore.LIGHTBLUE_EX}Password Strength: {Fore.LIGHTGREEN_EX}Very Strong",
+        20:f"{Fore.LIGHTBLUE_EX}Password Strength: {Fore.LIGHTGREEN_EX}Strong",
+        16:f"{Fore.LIGHTBLUE_EX}Password Strength: {Fore.GREEN}Good",
+        10:f"{Fore.LIGHTBLUE_EX}Password Strength: {Fore.LIGHTGREEN_EX}Great",
+        6 :f"{Fore.LIGHTBLUE_EX}Password Strength: {Fore.LIGHTRED_EX}Weak",
+        0 :f"{Fore.LIGHTBLUE_EX}Password Strength: {Fore.RED}Very Weak"
+    }
+    next((print(tmp[key]) for key in tmp.keys() if len(password) >= key), None)
+
+if __name__ == '__main__': start()
